af3dd0e3b2f69bf36a7374c1b5be61b01a8000e6
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-05-11 15:32:58 +0100

    Add event handlers and form submittal

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-class App extends React.Component {
+class App extends React.Component {  
   render() {
     return (
-      <div>
+      <div className="ui container">
         <SearchBar />
       </div>
     )
